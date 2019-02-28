59017bb14b397999ced1b0692169930afe53455d
sddanila <sddanila@yahoo.co.uk>
2019-02-28 20:16:14 +0000

    added seeded database with 3 rows of data in each table

--- routes/login.js
+++ routes/login.js
@@ -1,10 +1,19 @@
-"use strict";
+'use strict';
 
 const express = require('express');
 const router  = express.Router();
+const knex = require('knex')({
+  client: 'pg',
+  connection: {
+      host : '127.0.0.1',
+      user : 'labber',
+      password : 'labber',
+      database : 'midterm'
+  }
+});
 
 module.exports = (knex) => {
-
+const dbUtils = require('../lib/dbutils.js')(knex)
 /* Log In HTTP requests
 ---------------------------------------------
 */
@@ -17,8 +26,17 @@ module.exports = (knex) => {
 //Post From Form on Log in page
   router.post("/", (req, res) => {
     let info = req.body;
-    res.send("Post to login page" + info.email + info.password);
+    if(!dbUtils.findEmail(info.email)){
+      res.status(403).send('Email not found!');
+    } else if (!dbUtils.checkPassword(info.password)){
+      res.status(403).send('Wrong password!')
+    } else {
+      req.session.username = dbUtils.getUsername(info.email);
+      res.redirect('/');
+    }
+
   });
 
   return router;
 };
+
